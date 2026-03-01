e9cdfaf46377b2589e56d6260c22142f7970e063
gilenogestorfinanceiro <gilenopaivalima@gmail.com>
2026-03-01 17:28:13 -0300

    v1.0.0 - Service Worker atualizado

--- sw.js
+++ sw.js
@@ -1,16 +1,34 @@
-const CACHE_VERSION = '0.0.1';
+const CACHE_VERSION = '1.0.0';
 const CACHE_NAME = 'gestao-saude-' + CACHE_VERSION;
+const URLS_TO_CACHE = [
+    '/',
+    '/index.html'
+];
 
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['/'])));
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    );
     self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-    e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
     self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    e.respondWith(
+        fetch(e.request)
+            .then(response => {
+                const clone = response.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+                return response;
+            })
+            .catch(() => caches.match(e.request))
+    );
 });
